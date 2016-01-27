d3e5e5c20c747b8cf8846d184037ae5c6915cd1a
Ben Krieger <blitzrk@gmail.com>
2016-01-27 14:45:11 -0700

    Build from latest non-suffixed semver tag

--- build.sh
+++ build.sh
@@ -3,8 +3,18 @@
 git submodule init
 git submodule update
 
-(cd libsass && git pull origin master)
-(cd sassc.git && git pull origin master)
+(
+	cd libsass
+	git pull origin master
+	TAG=`git log --date-order --tags --simplify-by-decoration --pretty=format:"%d" | perl -ne 'print if s/.+?tag: v?([0-9.]+)[,\)].*$/\1/' | head -n 1`
+	git checkout tags/$TAG
+)
+(
+	cd sassc.git
+	git pull origin master
+	TAG=`git log --date-order --tags --simplify-by-decoration --pretty=format:"%d" | perl -ne 'print if s/.+?tag: v?([0-9.]+)[,\)].*$/\1/' | head -n 1`
+	git checkout tags/$TAG
+)
 
 BUILD="shared" make -C libsass -j5
 SASS_LIBSASS_PATH="`pwd`/libsass" make -C sassc.git -j5
