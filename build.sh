7e7565c3e415a40949cfc41c90fcdc57ad533863
Ben Krieger <blitzrk@gmail.com>
2016-02-01 15:47:16 -0700

    Make shell build script more readable and remove unnecessary fetching

--- build.sh
+++ build.sh
@@ -1,24 +1,12 @@
 #!/bin/bash
 
-git submodule init
-git submodule update
+git submodule --quiet init
+git submodule --quiet update
+git submodule foreach "git checkout tags/\`git log --date-order --tags --simplify-by-decoration --pretty=format:"%d" | perl -ne 'print if s/.+?tag: v?([0-9.]+)[,\)].*$/\1/' | head -n 1\`"
 
-(
-	cd libsass
-	git pull origin master
-	TAG=`git log --date-order --tags --simplify-by-decoration --pretty=format:"%d" | perl -ne 'print if s/.+?tag: v?([0-9.]+)[,\)].*$/\1/' | head -n 1`
-	git checkout tags/$TAG
-)
-(
-	cd sassc.git
-	git pull origin master
-	TAG=`git log --date-order --tags --simplify-by-decoration --pretty=format:"%d" | perl -ne 'print if s/.+?tag: v?([0-9.]+)[,\)].*$/\1/' | head -n 1`
-	git checkout tags/$TAG
-)
-
-BUILD="shared" make -C libsass -j5
-SASS_LIBSASS_PATH="`pwd`/libsass" make -C sassc.git -j5
+BUILD="shared" make -C libsass -j5 >/dev/null && echo "libsass build succeeded"
+SASS_LIBSASS_PATH="`pwd`/libsass" make -C sassc.git -j5 >/dev/null && echo "sassc build succeeded"
 
 cp sassc.git/bin/sassc ./
 
-echo -e "\n\nNow copy 'sassc' to the correct directories and commit.\n"
+echo -e "\nNow copy 'sassc' to the correct directories and commit.\n"
