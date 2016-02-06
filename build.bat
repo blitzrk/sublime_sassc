eee066bb1c2157a83ddd45006130237a6a1cfbbf
Ben Krieger <blitzrk@gmail.com>
2016-02-05 17:44:24 -0700

    Compile windows binaries with static C++ DLLs

--- build.bat
+++ build.bat
@@ -8,11 +8,13 @@ set git="%ProgramFiles%\Git\bin\git"
 :: s/14/12/ for VS2013 (min req) instead of VS2015 
 set msbuild="%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild"
 
+set cfg=/m /p:Configuration=Release
 if "%1"=="-m32" (
-	set cfg="/p:Platform=Win32"
+	set cfg=%cfg% /p:Platform=Win32
 ) else if "%1"=="-m64" (
-	set cfg="/p:Platform=Win64"
+	set cfg=%cfg% /p:Platform=Win64
 )
+set cfg=%cfg% /p:ForceImportBeforeCppTargets=%~dp0%static.props
 
 %git% submodule -q deinit -f .
 %git% submodule -q init
@@ -25,9 +27,9 @@ cmd /c "cd sassc.git && %git% checkout tags/%SASSC%" >nul
 xcopy sassc.git\* libsass\sassc /s /i >nul
 cmd /c (
 	cd libsass\sassc
-	%msbuild% win\sassc.sln /m /p:Configuration=Release %cfg% /t:Clean;Build"
+	%msbuild% win\sassc.sln %cfg% /t:Clean;Build
+	copy bin\sassc.exe ..\..\sassc.exe
 	cd ..\..
 )
 
-copy libsass\sassc\bin\sassc.exe sassc.exe >nul
 echo "Built sassc.exe. Copy to appropriate directories."
